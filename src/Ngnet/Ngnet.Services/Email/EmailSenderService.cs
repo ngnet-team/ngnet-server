ea11a5abf2d13f9985efae49d88eb21fb6447879
Dimitar <Dimitar>
2021-09-13 15:48:10 +0300

    Add email sender service

---
+++ src/Ngnet/Ngnet.Services/Email/EmailSenderService.cs
@@ -0,0 +1,45 @@
+﻿using Ngnet.Common;
+using Ngnet.Services.Email;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Ngnet.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        private readonly SendGridClient sender;
+
+        public EmailSenderService(string key)
+        {
+            this.sender = new SendGridClient(key);
+        }
+
+        public async Task<Response> SendEmailAsync(EmailSendetModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.FromAddress) && string.IsNullOrWhiteSpace(model.ToAddress))
+            {
+                throw new ArgumentException(ValidationMessages.EmptryEmailSenderAddresses);
+            }
+
+            var fromAddress = new EmailAddress(model.FromAddress, model.FromName);
+            var toAddress = new EmailAddress(model.ToAddress);
+
+            var mail = MailHelper.CreateSingleEmail(fromAddress, toAddress, model.Subject, null, model.Content);
+
+            Response response = null;
+
+            try
+            {
+                response = await this.sender.SendEmailAsync(mail);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/Ngnet/Ngnet.Services/Email/EmailSendetModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Ngnet.Services.Email
+{
+    public class EmailSendetModel
+    {
+        public string FromAddress { get; set; }
+
+        public string FromName { get; set; }
+
+        public string ToAddress { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+    }
+}
